269981cb4325366c3b89eac21699abdb36494b00
Or Levi <levior9@gmail.com>
2023-01-14 22:22:12 +0200

    Update PlayerNetwork.cs

--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 
 public class PlayerNetwork : NetworkBehaviour
 {
+
+    private NetworkVariable<int> randomNum = new NetworkVariable<int>(1, NetworkVariableReadPermission.Everyone,NetworkVariableWritePermission.Owner);
     private void Update()
     {
+        Debug.Log(OwnerClientId + ": Random Number: " + randomNum.Value);
         if (!IsOwner) return;
+
+        if (Input.GetKey(KeyCode.T))
+        {
+            randomNum.Value = Random.Range(0, 100);
+        }
         Vector3 moveDir = new Vector3(0, 0, 0);
         float moveSpeed = 3f;
 
